e26df56c410a3f0f98a659f0f55939e47cf6f0ce
Roli95 <torok.roland95@gmail.com>
2015-07-16 19:49:12 +0200

    ujra
    alma

--- src/csharp/CleverAlgorithms/Python Scripts/Algorithms/Random Search.py
+++ src/csharp/CleverAlgorithms/Python Scripts/Algorithms/Random Search.py
@@ -1,25 +1,29 @@
 import unittest
 
 try:
-    vector_temp=[]
-    i=0
-    k=0
-    n = int(raw_input("Adja meg a vektor hosszat: "))
-    while i<=n:
-        k = int(raw_input("Adjon meg egy vektorbeli erteket: "))
-        vector_temp.append(k)
-        i=i+1
-    ossz_ertek = int(raw_input("Adja meg a vegeredmenyt: "))
+    j = 0
+    l = int(raw_input("Adja meg a vektorok szamat: "))
+    while j<l:
+        vector_temp=[]
+        i=0
+        k=0
+        n = int(raw_input("Adja meg a vektor hosszat: "))
+        while i<n:
+            k = int(raw_input("Adjon meg egy vektorbeli erteket: "))
+            vector_temp.append(k)
+            i=i+1
+        j=j+1
+    ossz = int(raw_input("Adja meg a vegeredmeny erteket: "))
+        
     def Random_Search(vector):
         summa = 0
         for i in vector:
-                summa = summa+i**2
-        return summa
-        print summa
-        
+            summa = summa+i**2
+        return summa.random()
+            
     class MyTest(unittest.TestCase):
         def test(self):
-            self.assertEqual(Random_Search(vector_temp), ossz_ertek)
+            self.assertEqual(min(Random_Search(vector_temp)), ossz)
     if __name__=='__main__':
         unittest.main()
     
